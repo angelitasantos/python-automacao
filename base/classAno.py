608269e0071b1a2270122344752b51866fc8d7a5
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 15:56:33 -0300

    cria funcao pesquisar_tipo_processo

--- base/classAno.py
+++ base/classAno.py
@@ -12,3 +12,18 @@ class Ano:
 
     def __repr__(self):
         return self.ano
+
+    executar = EmpresaResult.criar_novas_pastas_arquivos(Base.self)
+
+    def pesquisar_tipo_processo(self):
+        try:
+            processo_ativo = None
+            busca = VarGerais.processo
+            for s in VarGerais.lista_processos:
+                if busca in s:
+                    processo_ativo = s
+                    break
+            if processo_ativo:
+                return processo_ativo
+        except:
+            Base.alertar_error_except(self, 'classAno', 'pesquisar_tipo_processo')
