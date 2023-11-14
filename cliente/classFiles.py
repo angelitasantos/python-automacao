c7a70da9263705a62944aa22bdf61df0ea200e04
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 16:47:45 -0300

    cria arquivo classFiles

---
+++ cliente/classFiles.py
@@ -0,0 +1,15 @@
+#----------------------------------------------------------------------------------------------------------------
+# ESCOLHER O ARQUIVO DE MODELO
+
+
+from cliente.classCliente import *
+
+
+class Files:
+
+    def __init__(self, arquivos):
+        self.arquivos = arquivos
+
+    def __repr__(self):
+        return self.excel
+    
