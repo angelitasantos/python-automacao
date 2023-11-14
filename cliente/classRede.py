866d7aae29a7b0957d559001d89cb10052a43f2f
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 16:24:45 -0300

    cria funcao escolher_tipo_comex

--- cliente/classRede.py
+++ cliente/classRede.py
@@ -22,4 +22,28 @@ class Rede:
     auditoria = 'SIM' if ProcResult.procedimento != 'AUDITORIA' else 'NÃO'
     proc_com_modal = pastas_em_massa == 'SIM' and pasta_modelo == 'SIM' and excel_em_massa == 'SIM' and pasta_comex == 'SIM' and arquivo_em_massa == 'SIM' and auditoria == 'SIM'
 
-    
+    def escolher_tipo_comex(self):
+        try:
+            if Ano.pesquisar_tipo_processo(self) == 'completo' and ProcResult.procedimento != Proc.procedimento4:
+                texto = 'Escolha uma Opção ...'
+                titulo = 'OPÇÃO'
+                botoes = [ 'IMPORTAÇÃO', 'EXPORTAÇÃO' ]
+                tipo_comex_nome = Base.confirmar_pyautogui(self, texto, titulo, botoes)
+                tipo_comex = 'E' if tipo_comex_nome == 'EXPORTAÇÃO' else 'I' if tipo_comex_nome == 'IMPORTAÇÃO' else 'Z'
+                return tipo_comex, tipo_comex_nome
+            
+            elif VarGerais.comex == 'IMPORTAÇÃO':
+                tipo_comex = 'I'
+                tipo_comex_nome = 'IMPORTAÇÃO'
+                return tipo_comex, tipo_comex_nome
+            
+            elif VarGerais.comex == 'EXPORTAÇÃO':
+                tipo_comex = 'E'
+                tipo_comex_nome = 'EXPORTAÇÃO'
+                return tipo_comex, tipo_comex_nome
+        except:
+            Base.alertar_error_except(self, 'classRede', 'escolher_tipo_comex')
+
+
+
+
