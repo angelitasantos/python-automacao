d5e5d9827df5213b48d80c6ccb293a3ae64d9a44
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 16:07:38 -0300

    cria variaveis class Proc

--- base/classProcedimento.py
+++ base/classProcedimento.py
@@ -12,3 +12,23 @@ class Proc:
 
     def __repr__(self):
         return self.ano
+
+    procedimento1 = 'CRIAR PASTAS/ARQUIVO'
+    procedimento2 = 'ATUALIZAR PLANILHA'
+    procedimento3 = 'HISTÓRICO'
+    procedimento4 = 'AUDITORIA'
+
+    subproc31 = 'PASTA DO PROCESSO'
+    subproc32 = 'CAPA DO PROCESSO'
+    subproc33 = 'PASTA MODELOS'
+    subproc34 = 'EXCEL EM MASSA'
+    subproc35 = 'PASTA COMEX'
+
+    subproc41 = 'EADI USIFAST'
+    subproc42 = 'SANTOS BRASIL'
+    subproc43 = 'BH AIRPORT'
+    subproc44 = 'GRU AIRPORT'
+    subproc45 = 'VCP AIRPORT'
+    subproc46 = 'CWB'
+
+
