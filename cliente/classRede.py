49358357e61542c5700914c37a43f67a203b24f4
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 16:23:04 -0300

    cria variavais class Rede

--- cliente/classRede.py
+++ cliente/classRede.py
@@ -14,6 +14,12 @@ class Rede:
     def __repr__(self):
         return self.excel
 
+    pastas_em_massa = 'SIM' if ProcResult.cod_proc != '12' else 'NÃO'
+    arquivo_em_massa = 'SIM' if ProcResult.cod_proc != '23' else 'NÃO'
+    pasta_modelo = 'SIM' if ProcResult.cod_proc != '33' else 'NÃO'
+    excel_em_massa = 'SIM' if ProcResult.cod_proc != '34' else 'NÃO'
+    pasta_comex = 'SIM' if ProcResult.cod_proc != '35' else 'NÃO'
+    auditoria = 'SIM' if ProcResult.procedimento != 'AUDITORIA' else 'NÃO'
+    proc_com_modal = pastas_em_massa == 'SIM' and pasta_modelo == 'SIM' and excel_em_massa == 'SIM' and pasta_comex == 'SIM' and arquivo_em_massa == 'SIM' and auditoria == 'SIM'
 
-
-
+    
