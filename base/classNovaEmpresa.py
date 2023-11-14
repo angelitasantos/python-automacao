e13a5a650a382c21a636d6aca23041da88e07a21
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 15:42:13 -0300

    cria funcao criar_nova_empresa

--- base/classNovaEmpresa.py
+++ base/classNovaEmpresa.py
@@ -12,3 +12,16 @@ class NovaEmpresa:
 
     def __repr__(self):
         return self.main
+
+    def criar_nova_empresa(self):
+        try:
+            if VarGerais.dir_rede == 'C:\\':
+                criar_caminho = True
+                return criar_caminho
+            else:
+                criar_caminho = False
+                return criar_caminho
+        except:
+            Base.alertar_error_except(self, 'classNovaEmpresa', 'criar_nova_empresa')
+
+
