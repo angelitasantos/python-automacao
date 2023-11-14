58d7a132580a235e8142d5e1a85c393e34070de2
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 15:43:39 -0300

    cria funcao criar_novos_caminhos

--- base/classNovaEmpresa.py
+++ base/classNovaEmpresa.py
@@ -24,4 +24,16 @@ class NovaEmpresa:
         except:
             Base.alertar_error_except(self, 'classNovaEmpresa', 'criar_nova_empresa')
 
+    def criar_novos_caminhos(self):
+        try:
+            Base.abrir_powershell(self)
+            Base.executar_comando_cd(self, VarGerais.dir_rede)
+            Base.executar_comando_mkdir(self, VarGerais.empresa)
+            Base.executar_comando_cd(self, VarGerais.empresa)
+            Base.executar_comando_mkdir(self, VarRede.pasta_arquivos_modelo)
+            Base.executar_comando_mkdir(self, VarSaude.pasta_financeiro)
+            Base.fechar_powershell(self)
+        except:
+            Base.alertar_error_except(self, 'classNovaEmpresa', 'criar_novos_caminhos')
 
+            
