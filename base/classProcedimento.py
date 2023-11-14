b6c0777e75f2f08dfaae8878f762c08591944a20
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 16:06:44 -0300

    cria arquivo classProcedimento

---
+++ base/classProcedimento.py
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------------------------------------------
+# ESCOLHER O PROCEDIMENTO
+
+
+from base.classAno import *
+
+
+class Proc:
+
+    def __init__(self, procedimento):
+        self.procedimento = procedimento
+
+    def __repr__(self):
+        return self.ano
