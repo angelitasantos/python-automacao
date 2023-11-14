1c3c6270a5bc43e6bb4fdbbfe12036e2e4f13e58
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 15:54:44 -0300

    cria arquivo classAno

---
+++ base/classAno.py
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------------------------------------------
+# ESCOLHER O ANO
+
+
+from base.classNovaEmpresa import *
+
+
+class Ano:
+
+    def __init__(self, ano):
+        self.ano = ano
+
+    def __repr__(self):
+        return self.ano
