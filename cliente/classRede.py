e801f42d7b9ea624b5f99a012599d53a07944a42
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 16:21:37 -0300

    cria arquivo classRede.py

---
+++ cliente/classRede.py
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------------------------------------------------------
+# DEFINIR O CAMINHO NA REDE
+
+
+from base.base import *
+from base.classProcedimento import *
+
+
+class Rede:
+
+    def __init__(self, rede):
+        self.rede = rede
+
+    def __repr__(self):
+        return self.excel
+
+
+
+
