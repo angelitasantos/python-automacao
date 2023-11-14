23bf97af12165dee219e6a0a892a2350dd7a1d73
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 15:41:05 -0300

    cria arquivo classNovaEmpresa.py

---
+++ base/classNovaEmpresa.py
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------------------------------------------
+# CRIAR PASTAS E GERAR ARQUIVOS MODELOS PARA NOVA EMPRESA
+
+
+from base.main import *
+
+
+class NovaEmpresa:
+
+    def __init__(self, main):
+        self.geral = main
+
+    def __repr__(self):
+        return self.main
