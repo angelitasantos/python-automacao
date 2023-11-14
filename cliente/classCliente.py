e2d2614b9aaea423448a76f7ecde2070fe3e85b0
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 16:37:31 -0300

    cria funcao escolher_sigla_comex

--- cliente/classCliente.py
+++ cliente/classCliente.py
@@ -41,5 +41,40 @@ class Cliente:
         except:
             Base.alertar_error_except(self, 'classCliente', 'digitar_ref_empresa')
 
+    def escolher_sigla_comex(self, tipo_comex):
+        try:
+            if Rede.proc_com_modal:
+                ref_filial_processo = VarGerais.filiais_exp if tipo_comex == 'E' else VarGerais.filiais_imp
 
+                if VarGerais.ref_filial == []:
+                    texto = 'Escolha a Filial ' + VarGerais.cliente + ' ...'
+                    titulo = 'OPÇÃO'
+                    botoes = [ item for item in VarGerais.filiais_nomes ]
+                    filial = Base.confirmar_pyautogui(self, texto, titulo, botoes)
+                    if filial != None:
+                        sigla_comex_index = VarGerais.filiais_nomes.index(filial)
+                        sigla_comex = ref_filial_processo[sigla_comex_index]
+                        return sigla_comex, sigla_comex_index
+                    else:
+                        sigla_comex = filial = 'Z'
+                        sigla_comex_index = 0
+                        return sigla_comex, sigla_comex_index 
+                else:
+                    if tipo_comex == 'I':
+                        filial = VarGerais.filiais_nomes[0]
+                        sigla_comex = VarGerais.filiais_imp[0]
+                        sigla_comex_index = VarGerais.filiais_imp.index(sigla_comex)
+                        return sigla_comex, sigla_comex_index
+                    
+                    elif tipo_comex == 'E':
+                        filial = VarGerais.filiais_nomes[0]
+                        sigla_comex = VarGerais.filiais_exp[0]
+                        sigla_comex_index = VarGerais.filiais_exp.index(sigla_comex)
+                        return sigla_comex, sigla_comex_index
+        except:
+            Base.alertar_error_except(self, 'classCliente', 'escolher_sigla_comex')
+
+
+
+            
     
