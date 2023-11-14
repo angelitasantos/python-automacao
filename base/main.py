c372206fa425f0e3f1c9900783bf7ef20d6116af
Angelita Santos <angelitasantos@gmail.com>
2023-11-14 15:33:55 -0300

    cria class VarGerais

--- base/main.py
+++ base/main.py
@@ -39,6 +39,56 @@ class ExcelExterno:
             exportados.append(cell_obj.value)
         return exportados
 
+class VarGerais:
+
+    def __init__(self, variaveis_geral):
+        self.variaveis_geral = variaveis_geral
+
+    def __repr__(self):
+        return self.variaveis_geral
+    
+    try:
+        dir_rede = ExcelExterno.var_gerais['C3'].value
+        dir_financeiro = ExcelExterno.var_gerais['C4'].value
+        grupo = ExcelExterno.var_gerais['C5'].value
+        lista_empresas =    [
+                                'EMPRESA 1',
+                                'EMPRESA 2',
+                                'EMPRESA 3'
+                            ]
+        empresa = ExcelExterno.var_gerais['C6'].value
+        cliente = ExcelExterno.var_gerais['C7'].value
+        apelido = ExcelExterno.var_gerais['C8'].value
+
+        filiais_nomes = ExcelExterno.listar_dados(Base.self, ExcelExterno.m_row, ExcelExterno.listas, 2)
+        filiais_imp = ExcelExterno.listar_dados(Base.self, ExcelExterno.m_row, ExcelExterno.listas, 3)
+        filiais_exp = ExcelExterno.listar_dados(Base.self, ExcelExterno.m_row, ExcelExterno.listas, 4)
+        ref_filial = []
+        ref_interna_imp = ExcelExterno.listar_dados(Base.self, ExcelExterno.m_row, ExcelExterno.listas, 5)
+        ref_interna_exp = ExcelExterno.listar_dados(Base.self, ExcelExterno.m_row, ExcelExterno.listas, 6)
+
+        pasta_interna_imp = ExcelExterno.var_gerais['C9'].value
+        pasta_interna_exp = ExcelExterno.var_gerais['C10'].value
+
+        total_tentativas = int(ExcelExterno.var_gerais['C11'].value)
+        tentativas = int(ExcelExterno.var_gerais['C12'].value)
+
+        lista_processos =   [
+                                'auditoria',
+                                'importação-desembaraço',
+                                'importação-pre-entry',
+                                'exportação-desembaraço',
+                                'exportação-pre-entry',
+                                'importação-histórico',
+                                'exportação-histórico',
+                                'completo'
+                            ]
+        processo = ExcelExterno.var_gerais['C13'].value
+        comex = ExcelExterno.var_gerais['C14'].value
+        movto = ExcelExterno.var_gerais['C15'].value
+    except:
+        Base.alertar_error_except(Base.self, 'main', 'VarGerais')
+
 
 
 
