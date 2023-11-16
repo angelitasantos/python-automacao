ff7bb780983a5b9a36a2b9cc0e0ecebc539596f8
Angelita Santos <angelitasantos@gmail.com>
2023-11-16 16:24:04 -0300

    cria arquivo classAcoes

---
+++ classAcoes.py
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------------------------------------------------------
+# ESCOLHER A AÇÃO A SER EXECUTADA
+
+
+from base.base import *
+from externos.classEmMassaXML import *
+
+
+class Acoes:
+
+    def __init__(self, acao):
+        self.acao = acao
+
+    def __repr__(self):
+        return self.acao
+    
+
+
+
