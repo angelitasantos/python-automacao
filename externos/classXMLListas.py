873620e823478431abc677209caf87b84c512306
Angelita Santos <angelitasantos@gmail.com>
2023-11-16 14:38:50 -0300

    cria funcao definir_listas_caminho_em_massa

--- externos/classXMLListas.py
+++ externos/classXMLListas.py
@@ -93,3 +93,92 @@ class XMLListas:
         except:
             Base.alertar_error_except(self, 'classXMLListas', 'criar_listas_em_massa')
 
+class XMLListasResult:
+
+    def __init__(self, xml):
+        self.xml = xml
+
+    def __repr__(self):
+        return self.xml
+
+    def definir_listas_caminho_em_massa(self):
+        try:
+            if CliResult.valida_cliente:
+                existe_arquivo_em_massa, arquivo_em_massa = Base.pesquisar_existe_arquivo(Base.self, VarRede.caminho_modelo, VarRede.arquivo_em_massa)
+                if existe_arquivo_em_massa and (ProcResult.cod_proc == '12' or ProcResult.cod_proc == '23'):
+                    existe_arquivo_capa, caminho_capa_modelo, diretorio, subdiretorio = XMLListas.listar_caminhos_arquivo_em_massa(Base.self)
+                    listas_caminho_em_massa = XMLListas.criar_listas_em_massa(Base.self, caminho_capa_modelo, diretorio, subdiretorio)
+                    lista_processos = listas_caminho_em_massa[7]
+                    qtd_processos = len(lista_processos)
+
+                elif ProcResult.cod_proc == '11' or ProcResult.cod_proc == '21':
+                    caminho_processo = FilesResult.processo + '\\'
+                    caminho_processo_pc = FilesResult.processo_pc + '\\'
+                    txt = f'{CliResult.dados_lista[0]} - DI.txt'
+                    xml = f'{CliResult.dados_lista[0]} - DI.xml'
+                    modal = CliResult.dados_lista[6]
+                    sigla_progress = CliResult.dados_lista[8]
+                    num_progress = CliResult.dados_lista[0]
+                    arquivo_capa = f'{VarRede.capa_base}{modal} {VarGerais.apelido} - {sigla_progress} - {num_progress}.xlsx'
+                    
+                    lista_caminho = [ caminho_processo ]
+                    lista_caminho_pc = [ caminho_processo_pc ]
+                    lista_arquivo_txt = [ txt ]
+                    lista_arquivo_xml = [ xml ]
+                    lista_sigla_pleno = [ CliResult.dados_lista[10] ]
+                    lista_num_pleno = [ CliResult.dados_lista[1] ]
+                    lista_sigla_progress = [ sigla_progress ]
+                    lista_num_progress = [ num_progress ]
+                    lista_arquivo_capa = [ arquivo_capa ]
+                    lista_capa_modelo = [ FilesResult.capa_modelo ]
+                    lista_processos = [ num_progress ]
+                    qtd_processos = len(lista_processos)
+
+                    listas_caminho_em_massa =   [
+                                                    lista_caminho,
+                                                    lista_caminho_pc,
+                                                    lista_arquivo_txt,
+                                                    lista_arquivo_xml,
+                                                    lista_sigla_pleno,
+                                                    lista_num_pleno,
+                                                    lista_sigla_progress,
+                                                    lista_num_progress,
+                                                    lista_arquivo_capa,
+                                                    lista_capa_modelo
+                                                ]
+                
+                else:
+                    listas_caminho_em_massa =   [
+                                                    'Z',
+                                                    'Z',
+                                                    'Z',
+                                                    'Z',
+                                                    'Z',
+                                                    'Z',
+                                                    'Z',
+                                                    'Z',
+                                                    'Z',
+                                                    'Z'
+                                                ]
+                    lista_processos = []
+                    qtd_processos = len(lista_processos)
+
+            else:
+                listas_caminho_em_massa =   [
+                                                'Z',
+                                                'Z',
+                                                'Z',
+                                                'Z',
+                                                'Z',
+                                                'Z',
+                                                'Z',
+                                                'Z',
+                                                'Z',
+                                                'Z'
+                                            ]
+                lista_processos = []
+                qtd_processos = len(lista_processos)
+            
+            return listas_caminho_em_massa, qtd_processos, lista_processos
+        except:
+            Base.alertar_error_except(self, 'classXMLListas', 'definir_listas_caminho_em_massa')
