99b939a9ef2ef16793b0c6cda6052abfd54c6c40
Angelita Santos <angelitasantos@gmail.com>
2023-11-16 16:25:18 -0300

    cria funcao escolher_proc_criar_pastas

--- classAcoes.py
+++ classAcoes.py
@@ -42,3 +42,34 @@ class Acoes:
                     time.sleep(Base.time_sleep_1)
         except:
             Base.alertar_error_except(self, 'classAcoes', 'escolher_procedimento')
+
+    def escolher_proc_criar_pastas(self, cod_procedimento):
+        try:
+            if cod_procedimento == '11':
+                if CliResult.valida_cliente:
+                    existe_arquivo_capa, arquivo_capa = Base.pesquisar_existe_arquivo(Base.self, FilesResult.processo_pc, FilesResult.capa_novo)
+
+                    if existe_arquivo_capa:
+                        mensagem = 'O arquivo deste processo já foi criado anteriormente !!!'
+                        Base.alertar_pyautogui(self, mensagem)
+                        Files.abrir_pasta_processo(self, FilesResult.processo)
+                    else:
+                        mouse_listener = pynput.mouse.Listener(suppress = True)
+                        mouse_listener.start()
+                        Base.abrir_powershell(self)
+                        Files.criar_nova_pasta(self, FilesResult.processo_pc)
+                        Files.copiar_novo_arquivo(self, FilesResult.processo_pc, FilesResult.capa_novo)
+                        Files.abrir_pasta_processo(self, FilesResult.processo)
+                        mouse_listener.stop()
+
+            elif cod_procedimento == '12':
+                texto = 'Confirma a criação das pastas em massa ?'
+                confirma_atualizacao = Base.confirmar_atualizacao(self, texto)
+                mouse_listener = pynput.mouse.Listener(suppress = True)
+                mouse_listener.start()
+                EmMassaCapa.criar_pastas_em_massa(self, confirma_atualizacao)
+                mouse_listener.stop()
+        except:
+            Base.alertar_error_except(self, 'classAcoes', 'abrir_proc_criar_pastas')
+
+            
