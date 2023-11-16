99eb245e479b3ff76623b9ed9f12b125e61505f9
Angelita Santos <angelitasantos@gmail.com>
2023-11-16 16:18:25 -0300

    cria funcao renomear_pasta

--- externos/classEmMassaCapa.py
+++ externos/classEmMassaCapa.py
@@ -18,3 +18,23 @@ class EmMassaCapa:
     except:
         Base.alertar_error_except(Base.self, 'classEmMassaCapa', 'EmMassaCapa')
     
+    def renomear_pasta(self, caminho_modelo, caminho_pc, arquivo_modelo, arquivo_novo):
+        try:
+            Base.abrir_powershell(self)
+            copy_item = f'copy "{caminho_modelo}" "{caminho_pc}"'
+            pyperclip.copy(copy_item)
+            time.sleep(Base.time_sleep_1)
+            Base.executar_hotkey_colar(self)
+            pasta_nova = caminho_pc + '\\' + arquivo_modelo
+            rename_item = f'Rename-Item "{pasta_nova}" "{arquivo_novo}"'
+            pyperclip.copy(rename_item)
+            time.sleep(Base.time_sleep_1)
+            Base.executar_hotkey_colar(self)
+            Base.fechar_powershell(self)
+        except:
+            Base.alertar_error_except(self, 'classEmMassaCapa', 'renomear_pasta')
+
+
+
+
+            
