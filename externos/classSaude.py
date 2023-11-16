bf288504ba12dcd8a322d3ef8e3fffe7b8c1d84b
Angelita Santos <angelitasantos@gmail.com>
2023-11-16 16:11:19 -0300

    cria funcao extrair_dados_saude

--- externos/classSaude.py
+++ externos/classSaude.py
@@ -33,3 +33,43 @@ class Saude:
         except:
             Base.alertar_error_except(self, 'classSaude', 'extrair_dados')
     
+    def extrair_dados_saude(self):
+        try:
+            df_banco1 = pd.read_excel   (
+                                            VarSaude.caminho_completo,
+                                            sheet_name = VarSaude.empresa_banco1,
+                                            usecols = VarSaude.require_cols,
+                                            skiprows = range(0, 1)
+                                        )
+            
+            df_banco2 = pd.read_excel   (
+                                            VarSaude.caminho_completo,
+                                            sheet_name = VarSaude.empresa_banco2,
+                                            usecols = VarSaude.require_cols,
+                                            skiprows = range(0, 1)
+                                        )
+
+            new_df_banco1 = Saude.extrair_dados(self, df_banco1, VarSaude.banco1)
+            new_df_banco2 = Saude.extrair_dados(self, df_banco2, VarSaude.banco2)
+
+            new_df = pd.merge(new_df_banco1, new_df_banco2, how = 'outer')
+            new_df['Data'] = pd.to_datetime(new_df.Data)
+            new_df = new_df.sort_values(by='Data')
+            new_df['Data'] = new_df['Data'].dt.strftime('%d/%m/%Y')
+            pd.set_option('float_format', '{:.2f}'.format)
+
+            df_completo = []
+            locale.setlocale(locale.LC_MONETARY, 'pt_BR.UTF-8')
+            for index, rows in new_df.iterrows():
+                rows.Credito = locale.currency(rows.Credito)
+                rows.Debito = locale.currency(rows.Debito)
+                rows.Credito = rows.Credito.replace('R$ ','')
+                rows.Debito = rows.Debito.replace('R$ ','')
+                my_list = [rows.Banco, rows.Data, rows.Credito, rows.Debito, rows.Despesas, rows.AR, rows.Tipo, rows.Numero, rows.NF]
+                df_completo.append(my_list)
+            return df_completo
+        except:
+            Base.alertar_error_except(self, 'classSaude', 'extrair_dados_saude')
+
+
+                
