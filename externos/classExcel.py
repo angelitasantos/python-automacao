457ebfd143aeff19e529846a72ff1a6e2e3117ff
Angelita Santos <angelitasantos@gmail.com>
2023-11-16 15:39:32 -0300

    cria funcao atualizar_dados_planilha

--- externos/classExcel.py
+++ externos/classExcel.py
@@ -34,7 +34,56 @@ class Excel:
                 lista_faturas.append(faturas)
         return lista_dados_gerais, lista_dados_adicoes, lista_conhecimentos, lista_qtd_faturas, lista_faturas
     
+    def atualizar_dados_planilha(self, indice, caminho=[], arquivo=[]):
+        try:
+            caminho_capa = caminho[indice] + arquivo[indice]
+            workbook = load_workbook(caminho_capa)
+            sheet = workbook['CAPA']
+            planilha_atualizada = sheet['C6'].value
+
+            lista_dados_gerais, lista_dados_adicoes, lista_conhecimentos, lista_qtd_faturas, lista_faturas = Excel.extrair_todos_dados_xml(Base.self, Excel.lista_root)
+
+            if planilha_atualizada == 'Não':
+                # dados processo
+                sheet['C2'] = lista_dados_gerais[indice][0]
+                sheet['C3'] = lista_dados_gerais[indice][1]
+                sheet['E3'] = lista_dados_gerais[indice][2]
+                sheet['C4'] = lista_dados_gerais[indice][3]
+                sheet['C7'] = AnoResult.ano_completo
+                sheet['D8'] = lista_dados_gerais[indice][5]
+                sheet['D9'] = lista_dados_gerais[indice][6]
+                sheet['C10'] = lista_dados_gerais[indice][7]
+                sheet['D10'] = lista_dados_gerais[indice][8]
+                sheet['H7'] = lista_dados_gerais[indice][9]
+                sheet['H8'] = lista_dados_gerais[indice][10]
+                sheet['D13'] = lista_dados_gerais[indice][11]
+                sheet['D22'] = lista_dados_gerais[indice][12]
+                sheet['I8'] = lista_dados_gerais[indice][13]
+                sheet['E2'] = lista_dados_gerais[indice][14]
+                sheet['C6'] = 'Sim'
+                sheet['E6'] = lista_dados_gerais[indice][14]
+                
+                # dados adições
+                sheet['C5'] = lista_dados_adicoes[indice][0]
+                sheet['D5'] = lista_dados_adicoes[indice][1]
+                sheet['D15'] = lista_dados_adicoes[indice][2]
+                sheet['E14'] = lista_dados_adicoes[indice][3]
+                sheet['C14'] = lista_dados_adicoes[indice][4]
+                sheet['I7'] = lista_dados_adicoes[indice][5]
+                sheet['L21'] = lista_dados_adicoes[indice][6]
+                sheet['M21'] = lista_dados_adicoes[indice][8]
+                sheet['I9'] = lista_dados_adicoes[indice][9]
+
+                # dados conhecimentos / faturas
+                sheet['D12'] = lista_conhecimentos[indice]
+                sheet['C17'] = lista_qtd_faturas[indice]
+                sheet['D17'] = lista_faturas[indice]
+                
+                workbook.save(caminho_capa)
+            else:
+                workbook.save(caminho_capa)
+        except:
+            Base.alertar_error_except(self, 'classExcel', 'atualizar_dados_planilha')
 
 
 
-    
