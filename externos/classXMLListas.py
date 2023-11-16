88e6b46e1f8c38e6469e4b980aae69332df61368
Angelita Santos <angelitasantos@gmail.com>
2023-11-16 14:30:43 -0300

    cria arquivo classXMLListas

---
+++ externos/classXMLListas.py
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------------------------------------------------------
+# LISTA DE DADOS CLIENTE E EMPRESA ARQUIVO EM MASSA
+
+
+from base.base import *
+from cliente.classFiles import *
+
+
+class XMLListas:
+
+    def __init__(self, xml):
+        self.xml = xml
+
+    def __repr__(self):
+        return self.xml
+    
+
+
+
+    
